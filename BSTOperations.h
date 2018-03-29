19b86d353a3ec48d7b083ab3951f24cf3fc73ee0
Shubham-tiwari123 <st540362@gmail.com>
2018-03-29 14:07:38 +0530

    Class file

--- BSTOperations.h
+++ BSTOperations.h
@@ -10,11 +10,23 @@ private:
     };
     node *root,*temp,*newnode,*prev;
     int num,info,k1,k2;
+    int temp1,flag1,flag2;
 public:
     void createBST();
-    void levelOrder(node *root);
+    void printInorder(struct node* newnode);
     void findMin();
     void printKeyInRange();
+    int makeTree();
+    void swapNode(struct node* roots);
+    
+    struct node *newNode(int data){
+        struct node *newnodes = new node;
+        newnodes->data = data;
+        newnodes->left = NULL;
+        newnodes->right = NULL;
+        return(newnodes);
+    }
+    
     BSTOperations(){
         root = NULL;
     }

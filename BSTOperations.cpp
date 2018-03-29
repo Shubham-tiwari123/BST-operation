c58ba3a031d2858e61a62b4701a15e91f133c989
Shubham-tiwari123 <st540362@gmail.com>
2018-03-29 22:29:12 +0530

    Function file

--- BSTOperations.cpp
+++ BSTOperations.cpp
@@ -1,43 +1,49 @@
 #include <iostream>
 #include<queue>
+#include <vector>
+#include<iterator>
+#include<algorithm>
 #include "BSTOperations.h"
 using namespace std;
 
 void BSTOperations::createBST(){
-    cout<<"Enter number of nodes:-";
+    cout<<"\nEnter number of nodes:-";
     cin>>num;
     for(int i=0;i<num;i++){
         cout<<"\nEnter the data:-";
         cin>>info;
-        
-        newnode = new node;
-        newnode->data = info;
-        newnode->left = NULL;
-        newnode->right = NULL;
-        
-        if(root == NULL)
-            root = newnode;
-        else{
-            temp=root;
-            while(temp!=NULL){
-                if (temp->data < info){
-                    prev = temp;
-                    temp = temp->right;
-                }
-                else{
-                    prev=temp;
-                    temp = temp->left;
-                }
-            }
-            if (prev->data < newnode->data) 
-                prev -> right = newnode;
-            else 
-                prev -> left = newnode;
-        }
+        addNode(info);
     }
     cout<<"\nDisplay BST:";
-    printInorder(root);
 }
+
+void BSTOperations::addNode(int data) {
+    newnode = new node;
+    newnode->data = info;
+    newnode->left = NULL;
+    newnode->right = NULL;
+
+    if(root == NULL)
+        root = newnode;
+    else{
+        temp=root;
+        while(temp!=NULL){
+            if (temp->data < info){
+                prev = temp;
+                temp = temp->right;
+            }
+            else{
+                prev=temp;
+                temp = temp->left;
+            }
+        }
+        if (prev->data < newnode->data) 
+            prev -> right = newnode;
+        else 
+            prev -> left = newnode;
+    }
+}
+
 
 void BSTOperations::findMin() {
     temp = root;
@@ -61,8 +67,6 @@ void BSTOperations::printKeyInRange() {
             if(q1.front()->right != NULL)
                  q2.push(q1.front()->right);
             
-            info = q1.front()->data;
-            
             if(k1 <= q1.front()->data && k2 >= q1.front()->data){
                 cout<<q1.front()->data<<" ";
             }
@@ -75,8 +79,6 @@ void BSTOperations::printKeyInRange() {
 
             if (q2.front()->right != NULL)
                 q1.push(q2.front()->right);
-            
-            info = q1.front()->data;
             
             if(k1 <= q2.front()->data && k2 >= q2.front()->data){
                 cout << q2.front()->data <<" ";
@@ -95,18 +97,14 @@ void BSTOperations::printInorder(struct node* newnode)
     printInorder(newnode->right);
 }
 
-int BSTOperations::makeTree() {
-    struct node *roots = newNode(6);
+int BSTOperations::makeTree(){
+    roots = newNode(6);
     roots->left        = newNode(10);
     roots->right       = newNode(2);
     roots->left->left  = newNode(1);
     roots->left->right = newNode(3);
     roots->right->right = newNode(12);
     roots->right->left = newNode(7);
-    cout<<endl;
-    cout<<"Display a general Tree:";
-    printInorder(roots);
-    swapNode(roots);
 }
 
 void BSTOperations::swapNode(struct node* roots){
@@ -163,7 +161,138 @@ void BSTOperations::swapNode(struct node* roots){
             q2.pop();
         }
     }
-    cout<<"\nDisplay BST:";
-    printInorder(roots);
+}
+
+void BSTOperations::mergeTwoBST(BSTOperations b1,BSTOperations b2) {
+    int i,j,k,m,n,x;
+    vector<int>v,v1;
+    vector<int>::iterator it;
+    queue <node *> q1,q2;
+    q1.push(b1.root);
+    while(!q1.empty() || !q2.empty()){
+        while(!q1.empty()){
+            if(q1.front()->left != NULL)
+                q2.push(q1.front()->left);
+
+            if(q1.front()->right != NULL)
+                 q2.push(q1.front()->right);
+            
+            v.push_back(q1.front()->data);
+            q1.pop();
+        }
+        while (!q2.empty())
+        {
+            if (q2.front()->left != NULL)
+                q1.push(q2.front()->left);
+
+            if (q2.front()->right != NULL)
+                q1.push(q2.front()->right);
+            
+            v.push_back(q2.front()->data);
+            q2.pop();
+        }
+    }
+    
+    q1.push(b2.root);
+    while(!q1.empty() || !q2.empty()){
+        while(!q1.empty()){
+            if(q1.front()->left != NULL)
+                q2.push(q1.front()->left);
+
+            if(q1.front()->right != NULL)
+                 q2.push(q1.front()->right);
+            
+            v1.push_back(q1.front()->data);
+            q1.pop();
+        }
+        while (!q2.empty())
+        {
+            if (q2.front()->left != NULL)
+                q1.push(q2.front()->left);
+
+            if (q2.front()->right != NULL)
+                q1.push(q2.front()->right);
+            
+            v1.push_back(q2.front()->data);
+            q2.pop();
+        }
+    }
+    sort(v.begin(),v.end());
+    sort (v1.begin(),v1.end());
+    
+    m = v.size();
+    n = v1.size();
+    int v3[m+n];
+    i=0,j=0,k=0;
+    cout<<"\nmerging two list:";
+    while (i < m && j < n){
+        if (v[i] < v1[j])
+        {
+            v3[k] = v[i];
+            i++;
+        }
+        else
+        {
+            v3[k] = v1[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < m){
+         v3[k] = v[i];
+         i++; k++;
+    }
+    while (j < n){
+        v3[k] = v1[j];
+        j++;k++;
+    }
+    root = NULL;
+    for(int a=0;a<(m+n);a++){
+        info = v3[a];
+        addNode(info);
+    }
+}
+
+void BSTOperations::largestBSTSubtree(struct node *temp1,int x) {
+    int count[2]={1,1};
+    int n,m;
+    queue <node *> q1,q2;
+    temp = temp1;
+    q1.push(temp);
+    while(!q1.empty() || !q2.empty()){
+        while(!q1.empty()){
+            if(q1.front()->left != NULL){
+               q2.push(q1.front()->left);
+               count[x]++;
+            }
+            if(q1.front()->right != NULL){
+                q2.push(q1.front()->right);
+                count[x]++;
+            }
+            q1.pop();
+        }
+        while (!q2.empty())
+        {
+            if (q2.front()->left != NULL){
+                q1.push(q2.front()->left);
+                count[x]++;
+            }
+            if (q2.front()->right != NULL){
+                q1.push(q2.front()->right);
+                count[x]++;
+            }
+            q2.pop();
+        }
+    }
+    if(x==0){
+        n = count[0];
+    }
+    else if(x==1){
+        m = count[1];
+        if(n>m)
+            cout<<"\nLeft subtree has more elements:-"<<n;
+        else
+            cout<<"\nRight subtree has more elements:-"<<m;
+    }
 }
 

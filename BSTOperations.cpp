83e586eb243bbb8b9b4e04ab66e6da985dab65e8
Shubham-tiwari123 <st540362@gmail.com>
2018-03-29 14:06:02 +0530

    Function file

--- BSTOperations.cpp
+++ BSTOperations.cpp
@@ -35,34 +35,8 @@ void BSTOperations::createBST(){
                 prev -> left = newnode;
         }
     }
-    levelOrder(root);
-}
-
-void BSTOperations::levelOrder(node *root){
-    queue <node *> q1,q2;
-    q1.push(root);
-    while(!q1.empty() || !q2.empty()){
-        while(!q1.empty()){
-            if(q1.front()->left != NULL)
-                q2.push(q1.front()->left);
-
-            if(q1.front()->right != NULL)
-                 q2.push(q1.front()->right);
-            cout<<q1.front()->data<<" ";
-            q1.pop();
-        }
-        while (!q2.empty())
-        {
-            if (q2.front()->left != NULL)
-                q1.push(q2.front()->left);
-
-            if (q2.front()->right != NULL)
-                q1.push(q2.front()->right);
-
-            cout << q2.front()->data <<" ";
-            q2.pop();
-        }
-    }
+    cout<<"\nDisplay BST:";
+    printInorder(root);
 }
 
 void BSTOperations::findMin() {
@@ -74,11 +48,11 @@ void BSTOperations::findMin() {
 }
 
 void BSTOperations::printKeyInRange() {
-    temp = root;
+
     cout<<"\nEnter two keys:-";
     cin>>k1>>k2;
     queue <node *> q1,q2;
-    q1.push(temp);
+    q1.push(root);
     while(!q1.empty() || !q2.empty()){
         while(!q1.empty()){
             if(q1.front()->left != NULL)
@@ -90,7 +64,6 @@ void BSTOperations::printKeyInRange() {
             info = q1.front()->data;
             
             if(k1 <= q1.front()->data && k2 >= q1.front()->data){
-                cout<<"yo";
                 cout<<q1.front()->data<<" ";
             }
             q1.pop();
@@ -106,7 +79,6 @@ void BSTOperations::printKeyInRange() {
             info = q1.front()->data;
             
             if(k1 <= q2.front()->data && k2 >= q2.front()->data){
-                cout<<"hi";
                 cout << q2.front()->data <<" ";
             }
             q2.pop();
@@ -114,4 +86,84 @@ void BSTOperations::printKeyInRange() {
     }
 }
 
+void BSTOperations::printInorder(struct node* newnode)
+{
+    if (newnode == NULL)
+        return;
+    printInorder(newnode->left);
+    cout<<newnode->data<<" ";
+    printInorder(newnode->right);
+}
+
+int BSTOperations::makeTree() {
+    struct node *roots = newNode(6);
+    roots->left        = newNode(10);
+    roots->right       = newNode(2);
+    roots->left->left  = newNode(1);
+    roots->left->right = newNode(3);
+    roots->right->right = newNode(12);
+    roots->right->left = newNode(7);
+    cout<<endl;
+    cout<<"Display a general Tree:";
+    printInorder(roots);
+    swapNode(roots);
+}
+
+void BSTOperations::swapNode(struct node* roots){
+    cout<<"\nSwapping nodes to make a proper BST:-";
+    flag1=0,flag2=0;
+    queue <node *> q1,q2;
+    q1.push(roots);
+    while(!q1.empty() || !q2.empty()){
+        while(!q1.empty()){
+            if(q1.front()->left != NULL)
+                flag1=1;
+
+            if(q1.front()->right != NULL)
+                flag2 =1;
+            if(flag1==1 && flag2==1){
+                if(q1.front()->left->data > q1.front()->data && q1.front()->right->data < q1.front()->data){
+                    temp1 = q1.front()->left->data;
+                    q1.front()->left->data = q1.front()->right->data;
+                    q1.front()->right->data = temp1;
+                }
+            }
+            if(flag1==1){
+                q2.push(q1.front()->left);
+                flag1=0;
+            }
+            if(flag2==1){
+                q2.push(q1.front()->right);
+                flag2=0;
+            }
+            q1.pop();
+        }
+        while (!q2.empty())
+        {
+            if(q2.front()->left != NULL)
+                flag1=1;
+
+            if(q2.front()->right != NULL)
+                flag2 =1;
+            if(flag1==1 && flag2==1){
+                if(q2.front()->left->data > q2.front()->data && q2.front()->right->data < q2.front()->data){
+                    temp1 = q2.front()->left->data;
+                    q2.front()->left->data = q2.front()->right->data;
+                    q2.front()->right->data = temp1;
+                }
+            }
+            if(flag1==1){
+                q1.push(q2.front()->left);
+                flag1=0;
+            }
+            if(flag2==1){
+                q1.push(q2.front()->right);
+                flag2=0;
+            }
+            q2.pop();
+        }
+    }
+    cout<<"\nDisplay BST:";
+    printInorder(roots);
+}
 
